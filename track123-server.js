21b7d869de5e7e49f4f36166d28df32c5d44cedd
Your Name <gospelachuenu@gmail.com>
2025-10-04 18:10:04 +0100

    Fix fetch import for Node.js compatibility in Track123 server

--- track123-server.js
+++ track123-server.js
@@ -3,7 +3,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
 const PORT = process.env.PORT || 3001;
